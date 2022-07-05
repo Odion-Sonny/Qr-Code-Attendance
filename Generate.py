c28c6a16a0565cdfefc8bf52c5e190cb14a4efc4
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-07-05 20:34:24 +0100

    Update Generate.py

--- Generate.py
+++ Generate.py
@@ -1,10 +1,12 @@
 from MyQR import myqr
 import os
 
+# read student name/id from students.txt
 f = open('students.txt','r')
 lines = f.read().split("\n")
 print(lines)
 
+# encode name/id in QR and generate QR
 for _ in range (0,len(lines)):
     data = lines[_]
     version,level,qr = myqr.run(

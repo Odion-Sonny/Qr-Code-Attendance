b02b5f20629da1d0d01ad209ca8d6685cb843089
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-07-05 20:32:33 +0100

    Update Attend.py

--- Attend.py
+++ Attend.py
@@ -7,6 +7,7 @@ import tkinter as tk
 from tkinter import Frame, ttk, messagebox
 from tkinter import *
 
+# Build GUI
 window = tk.Tk()
 window.title('Attendance System V-089')
 window.geometry('900x600') 
@@ -23,26 +24,33 @@ title.pack(side=tk.TOP,fill=tk.X)
 Manage_Frame=Frame(window,bg="lavender")
 Manage_Frame.place(x=0,y=80,width=480,height=530)
 
+# year input
 ttk.Label(window, text = "Year",background="lavender", foreground ="black",font = ("Times New Roman", 15)).place(x=100,y=150)
 combo_search=ttk.Combobox(window,textvariable=year,width=10,font=("times new roman",13),state='readonly')
 combo_search['values']=('1','2','3','4') 
 combo_search.place(x=250,y=150)
 
+# branch input
 ttk.Label(window, text = "Branch",background="lavender", foreground ="black",font = ("Times New Roman", 15)).place(x=100,y=200)
 combo_search=ttk.Combobox(window,textvariable=branch,width=10,font=("times new roman",13),state='readonly')
 combo_search['values']=("CSE","ECE","EEE","IT","MECH","ECM")
 combo_search.place(x=250,y=200)
 
+
+# section input
 ttk.Label(window, text = "Section",background="lavender", foreground ="black",font = ("Times New Roman", 15)).place(x=100,y=250)
 combo_search=ttk.Combobox(window,textvariable=sec,width=10,font=("times new roman",13),state='readonly')
 combo_search['values']=('A','B','C','D')
 combo_search.place(x=250,y=250)
 
+# period input
 ttk.Label(window, text = "Period",background="lavender", foreground ="black",font = ("Times New Roman", 15)).place(x=100,y=300)
 combo_search=ttk.Combobox(window,textvariable=period,width=10,font=("times new roman",13),state='readonly')
 combo_search['values']=('1','2','3','4','5','6','7')
 combo_search.place(x=250,y=300)
 
+
+# check that all fields have been field
 def checkk():
     if(year.get() and branch.get() and period.get() and sec.get()):
         window.destroy()
@@ -66,6 +74,8 @@ window.mainloop()
 
 cap = cv2.VideoCapture(0)
 names=[]
+
+# create excel file
 today=date.today()
 d= today.strftime("%b-%d-%Y")
 
@@ -84,11 +94,13 @@ def enterData(z):
         names.append(z)
         z=''.join(str(z))
         intime = it.strftime("%H:%M:%S")
-        fob.write(z+'\t'+branch.get()+'-'+sec.get()+'\t'+year.get()+'\t'+period.get()+'\t'+intime+'\n')
+        # log attendance details in excel file
+        fob.write(z+'\t'+branch.get()+'-'+sec.get()+'\t'+year.get()+'\t'+period.get()+'\t'+intime+'\n') 
     return names 
     
 print('Reading...')
 
+# check QR data
 def checkData(data):
     # data=str(data)    
     if data in names:
